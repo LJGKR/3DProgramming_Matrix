4b5c3c9572b3a419d6bce6bbd8285bad8618037e
진규 이 <wlsrb9086@naver.com>
2022-11-14 21:22:01 +0900

    [JG] 행렬 구조, 연산 구현

--- 행렬/Matrix.cpp
+++ 행렬/Matrix.cpp
@@ -1,12 +1,18 @@
 #include "MuSoenMath.h"
 #include <math.h>
 #include <iostream>
+
 using namespace std;
 
 void main() {
 	Vec3 v;
-	Mat3 mat;
+	Mat3 Scale;
 
-	operate();
+	v = Vec3(1, 1);
+	Scale = Mat3(3,0,0,
+		       0,5,0,
+		       0,0,1);
+
+	Vec3 k = v * Scale;
 
 }
--- 행렬/MuSoenMath.h
+++ 행렬/MuSoenMath.h
@@ -2,80 +2,78 @@
 #include <iostream>
 using namespace std;
 
-class Vec3
+struct Vec3
 {
-private :
-	
-
-public :
-	float v[1][3] =                  //백터 만들기
-	{
-		{2,2,1}
-	}; 
-	float a[1][3];                  //Translate과 곱한 백터 값을 넣을 배열
-	float b[1][3];                  //Rotate와 곱한 백터 값을 넣을 배열
-	float c[1][3];                  //Scale과 곱한 백터 값을 넣을 배열
-
-};
-
-class Mat3
-{
-	
-public:
-	float Translate[3][3] = {                        //이동행렬 구현 (값은 지정)
-		{1, 0, 0},
-		{0, 1, 0},
-		{3, 5, 1}
-	};
-
-	float Scale[3][3] = {                           //크기변환 행렬 구현(값 지정)
-		{2, 0, 0},
-		{0, 2, 0},
-		{0, 0, 1} 
-	};
-
-	float Rotate[3][3] = {                           //회전행렬 구현(값 지정)
-		{0.15,-0.5, 0},
-		{0.5,0.15, 0},
-		{0, 0, 1}
-	};
 private:
 
-};
-Vec3 vec;       //함수에서 사용할 백터 선언
-Mat3 mat;       //함수에서 사용할 매트릭스
+public:
 
-void operate()
+	int k = 3;
+	float vec3[3][1];
+	float vec[3][1];
+
+	Vec3() {
+		vec3[0][0] = 0, vec3[1][0] = 0, vec[2][0] = 0;
+	}
+
+	Vec3(float v1, float v2, int i = 1) {
+		vec3[0][0] = v1, vec3[1][0] = v2, vec3[2][0] = i;
+	}
+
+	Vec3& operator*(const Mat3& ref) {                     //백터와 행렬의 연산
+
+		if (k == 3) {
+			this->vec[0][0] = vec3[0][0] * ref.mat3[0][0] + vec3[1][0] * ref.mat3[1][0] + vec3[2][0] * ref.mat3[2][0];
+			this->vec[1][0] = vec3[0][0] * ref.mat3[0][1] + vec3[1][0] * ref.mat3[1][1] + vec3[2][0] * ref.mat3[2][1];
+			this->vec[2][0] = vec3[0][0] * ref.mat3[0][2] + vec3[1][0] * ref.mat3[1][2] + vec3[2][0] * ref.mat3[2][2];
+
+			this->vec3[0][0] = vec[0][0], this->vec3[1][0] = vec[1][0], this->vec3[2][0] = vec[2][0];
+
+			return *this;
+		}
+		if (k == 1) {
+
+		}
+	}
+};
+
+
+
+struct Mat3
 {
-	for (int i = 0; i < 1; i++) {
+private:
+
+public:
+	float mat3[3][3];
+
+
+	Mat3() {                     //값 없이 선언만 했을때 기본적으로 행렬에 들어가는 값
+		mat3[0][0] = 0, mat3[0][1] = 0, mat3[0][2] = 0,
+		mat3[1][0] = 0, mat3[1][1] = 0, mat3[1][2] = 0,
+		mat3[2][0] = 0, mat3[2][1] = 0, mat3[2][2] = 0;
+	};
+
+	Mat3(float m1, float m2, float m3,float m4, float m5, float m6,
+		float m7, float m8, float m9) {
+		mat3[0][0] = m1, mat3[0][1] = m2, mat3[0][2] = m3,
+		mat3[1][0] = m4, mat3[1][1] = m5, mat3[1][2] = m6,
+		mat3[2][0] = m7, mat3[2][1] = m8, mat3[2][2] = m9;
+	};
+
+	void Reset() {                //행렬값을 0으로 초기화 시키는 함수
+		for (int i = 0; i < 3; i++) {
 			for (int k = 0; k < 3; k++) {
-				for (int j = 0; j < 3; j++) {
-					vec.a[i][k] += vec.v[i][j] * mat.Translate[j][k];           //포문을 사용하여 계산한 값을 넣을 백터 (0,0)부터 시작하여 →↓순으로 곱해주면서 차례대로 a에 더하여 대입해줌
-				}
+				mat3[i][k] = 0;
 			}
-	}
-	/*--------------------------------------------------------------*/
-	for (int i = 0; i < 1; i++) {
-		for (int k = 0; k < 3; k++) {
-			for (int j = 0; j < 3; j++) {
-				vec.b[i][k] += vec.a[i][j] * mat.Rotate[j][k];
-			}                                                                    //이동행렬과 동일
-		} 
-	}
-	//----------------------------------------------------------------
-	for (int i = 0; i < 1; i++) {
-		for (int k = 0; k < 3; k++) {
-			for (int j = 0; j < 3; j++) {
-				vec.c[i][k] += vec.b[i][j] * mat.Scale[j][k];
-			}                                                                    //회전행렬과 동일
 		}
 	}
-	//----------------------------------------------------------------
-	cout << "Vec3 p의 좌표 : \n";
-	for (int i = 0; i < 1; i++) {
-		for (int j = 0; j < 3; j++) {
-			cout << "{" << vec.c[i][j] << "}";                                    //백터 좌표를 출력해주는 코드
+
+	void Identity() {             //단위행렬 초기화 함수
+		for (int i = 0; i < 3; i++) {
+			for (int k = 0; k < 3; k++) {
+				mat3[i][k] = 0;
+			}
 		}
-		cout << "   ";
+		mat3[0][0] = 1, mat3[1][1] = 1, mat3[2][2] = 1;
 	}
-}
+};
